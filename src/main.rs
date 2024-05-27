6d5dc66e42cf22c6e630755bbb99581bc9e57d10
Florian <fd@0x6664.com>
2024-05-27 13:05:52 +0700

    feat: repl

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+const PROMPT: &str  = "$ ";
+
 fn main() {
-    print!("$ ");
+    print!("{}", PROMPT);
     io::stdout().flush().unwrap();
 
     // Wait for user input
     let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
-    let cmd = input.trim();
-
-    if cmd.is_empty() {
-        println!("No command entered");
-    } else {
-        println!("{}: command not found", cmd);
+    loop {
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let cmd = input.trim();
+        if !cmd.is_empty() {
+            match cmd {
+                "exit" => break,
+                _ => println!("{}: command not found", cmd)
+            }
+        }
+        io::stdout().flush().unwrap();
+        print!("{}", PROMPT);
+        io::stdout().flush().unwrap();
     }
 }
